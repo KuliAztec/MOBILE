c34c01550905ae9829e5b9e9ac233c93d622093a
KuliAztec <muhammad.setyoko@mhs.unsoed.ac.id>
2025-09-21 22:07:52 +0700

    Kumat

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.example.ifunsoedmobile"
     compileSdk = 36
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.example.ifunsoedmobile"
         minSdk = 24
---
+++ readme.md
@@ -0,0 +1,9 @@
+#   Muhammad Azka Mauzaky Setyoko
+##  H1D023066
+### Praktikum Pemrograman Mobile Shift D
+
+# P1 
+<img src="Gambar/sakrinsut1.jpg" width="300" alt="P1">  
+
+# P2
+<img src="Gambar/sakrinsut2.jpg" width="300" alt="P2">
